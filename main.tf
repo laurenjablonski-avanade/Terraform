90389cc831925471ae99c8e45a5e47a51a22568b
laurenjablonski-avanade <103124400+laurenjablonski-avanade@users.noreply.github.com>
2024-05-14 16:18:39 +0100

    Update main.tf
    Linking the names to the variables.tf file

--- main.tf
+++ main.tf
@@ -5,22 +5,23 @@ provider "azurerm" {
 
 # Define a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "testing-terraform-rg"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # Define a virtual network
 resource "azurerm_virtual_network" "example" {
-  name                = "testing-terraform-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 # Define a subnet
 resource "azurerm_subnet" "example" {
-  name                 = "testing-terraform-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes     = var.subnet_address_prefix
 }
+
