e30810a36d652c3b337c252c1847f0cbd7075971
laurenjablonski-avanade <103124400+laurenjablonski-avanade@users.noreply.github.com>
2024-06-03 16:01:18 +0100

    Update main.tf
    only create a resource group first of all to make it simpler

--- main.tf
+++ main.tf
@@ -10,18 +10,18 @@ resource "azurerm_resource_group" "example" {
 }
 
 # Define a virtual network
-resource "azurerm_virtual_network" "example" {
-  name                = var.vnet_name
-  address_space       = var.vnet_address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
+#resource "azurerm_virtual_network" "example" {
+  #name                = var.vnet_name
+  #address_space       = var.vnet_address_space
+  #location            = var.location
+  #resource_group_name = var.resource_group_name
+#}
 
 # Define a subnet
-resource "azurerm_subnet" "example" {
-  name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
-  address_prefixes     = var.subnet_address_prefix
-}
+#resource "azurerm_subnet" "example" {
+  #name                 = var.subnet_name
+  #resource_group_name  = var.resource_group_name
+  #virtual_network_name = var.vnet_name
+  #address_prefixes     = var.subnet_address_prefix
+#}
 
